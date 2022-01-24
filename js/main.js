92cfa14ba785f057c8055e9dc60465596d491259
Numonbek <numonbek1996@gmail.com>
2022-01-25 00:16:26 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 import { touchSlide } from './modules/touchSlide.js';
 import { pages } from './modules/pages.js';
 import { clickSlide } from './modules/clickSlide.js';
-// import { habrSlide } from './modules/habrSlide.js';
 
 window.addEventListener('DOMContentLoaded', (e) => {
   pages();
   clickSlide();
   touchSlide();
-  // habrSlide();
 });
